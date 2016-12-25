17fbf4e417b37d2825e1a271eadd19f74c830936
Kiankris Villagonzalo <kcv@utexas.edu>
2016-12-24 18:34:05 -0600

    created booleans for better readability

--- TipCalculator.php
+++ TipCalculator.php
@@ -1,33 +1,13 @@
  <!DOCTYPE html>
  <html>
- <style type="text/css">
- 	body{
-		padding:5px;
-		text-align:center;
-	}	
- 	h1{
-		text-align:center;
-	}
- 	p{
-		text-align:center;
-	}
-	input{
-		text-align:center;
-	}
-	span{
-		display:inline-block;
-		padding:5px;
-		border:5px solid cyan;
-		font-size:140%;
-		font-weight:bold;
-		text-align:center;
-	}
- </style>
+ <head>
+ 	<link rel="stylesheet" href="styles.css">
+ </head>
 	<body>
 	<?php 
 		$subtotal = $custom_percentage = '';
 		$percentage = $tip = $total = 0;
-		$set = false;
+		$subtotal_format = $set = $cust_perc_format = false;
 
 		function calculatePercentage($value='')
 		{
@@ -40,13 +20,17 @@
 		// with 1,2 numbers, and the radio button is set
 		
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			if (preg_match('/^[0-9]+(?:\.[0-9]{0,2})?$/', $_POST["subtotal"])){
+			$subtotal_format = preg_match('/^[0-9]+(?:\.[0-9]{0,2})?$/', $_POST["subtotal"]);
+			$cust_perc_format = preg_match('/^[0-9]+(?:\.[0-9]{0,2})?$/', $_POST["custom_percentage"]);
+			
+			if ($subtotal_format){
 				if(!empty($_POST["percentage"])){
 					$percentage = calculatePercentage($_POST["percentage"]);
 					$subtotal = (float) $_POST["subtotal"];
 					$set = true;
 				}
-				elseif (!empty($_POST["custom_percentage"]) && preg_match('/^[0-9]+(?:\.[0-9]{0,2})?$/', $_POST["custom_percentage"]) ) {
+				elseif ($cust_perc_format) {
+					$custom_percentage = $_POST["custom_percentage"]; 
 					$percentage = calculatePercentage($_POST["custom_percentage"]);
 					$subtotal = (float) $_POST["subtotal"];
 					$set = true;
@@ -59,15 +43,14 @@
 			<h1>Tip Calculator</h1>
 			<p>Bill subtotal: $ <input type="text" name="subtotal" value="<?php echo $subtotal;?>"></p>
 			<?php 
-				$values = array("10", "15", "20", "-1") ;
-				for($i = 0; $i < 4; $i++){
-					$val = $values[$i]; 
-					
+				$values = array("10", "15", "20") ;
+				for($i = 0; $i < 4; $i++){					
 					if($i < 3){
+						$val = $values[$i]; 
 						?>
-						<input type="radio" name ="percentage" value="<?php echo $val; ?>" 
+						<input type="radio" name="percentage" value="<?php echo $val; ?>" 
 							<?php 
-							if (isset($percentage) && ($percentage * 100)==$val) 
+							if (isset($percentage) && ($percentage * 100)==$val)
 								echo "checked";
 							?>
 						> <?php echo $val; ?>%
@@ -76,32 +59,34 @@
 					else{
 						echo "</br>";
 						?>
-						<input type="radio" name="percentage" value="<?php echo $custom_percentage; ?>" class="more"> 
+						<input type="radio" name="percentage" value="<?php echo $custom_percentage; ?>" class="more"
 							<?php 
-								if (isset($custom_percentage) && ($custom_percentage * 100)==$val)
+								if ($custom_percentage != '')
 									echo "checked";
 							?>
+						> 
+							
 						<input type="text" name="custom_percentage" value="<?php echo $custom_percentage; ?>" 
 						<?php echo $val . "</br>";?>%
 						<?php	
 					}	
 				}
 			?>
-			<p><input type="submit" font= ></p>
+			<p><input type="submit"></p>
 		</form>
 
 		<?php
 			if($set){
-					$tip = $subtotal * $percentage;
-					$total = $subtotal + $tip ;
+					$tip = number_format((float) ($subtotal * $percentage), 2, '.', '');
+					$total = number_format((float) ($subtotal + $tip ), 2, '.', '');
 				?>
 				<p>
-				<span> 
-					<?php
-						echo "Tip: $" . $tip . "</br>" ;
-						echo "Total: $" . $total . "</br>" ;
-					?>
-				</span>
+					<span> 
+						<?php
+							echo "Tip: $" . $tip . "</br>" ;
+							echo "Total: $" . $total . "</br>" ;
+						?>
+					</span>
 				</p>
 				<?php
 			}
